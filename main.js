e9f968f852a30076ab3d2b0915a800f63ee99775
Eric van der Vlist <vdv@dyomedea.com>
2026-07-29 10:12:50 +0000

    Improve mobile presentation display

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ import "impress.js/js/impress.js";
 window.addEventListener("DOMContentLoaded", () => {
   impress().init();
 });
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register(`${import.meta.env.BASE_URL}sw.js`);
+  });
+}
